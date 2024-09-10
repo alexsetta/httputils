1b88fbf592bf119ac602b63f88387553238053cb
alexsetta <alexsetta@gmail.com>
2024-09-10 17:21:01 -0300

    mensagem de erro

--- httputils.go
+++ httputils.go
@@ -20,21 +20,21 @@ var (
 func GetBody(url string, debug bool) (string, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", fmt.Errorf("GetBody: #{erro}")
+		return "", fmt.Errorf("GetBody: NewRequest: %v", err)
 	}
 	res, err := client.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("GetBody: #{erro}")
+		return "", fmt.Errorf("GetBody: Do: %v", err)
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return "", fmt.Errorf("GetBody: status error: #{res.Status}")
+		return "", fmt.Errorf("GetBody: status error: %s", res.Status)
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return "", fmt.Errorf("GetBody: #{erro}")
+		return "", fmt.Errorf("GetBody: ReadAll: %v", err)
 	}
 	data := string(body)
 	if debug {
